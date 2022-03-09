eec11d093dea883221ce7e6c749254869270e299
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-03-09 12:01:11 +0300

    New task 7 done (table view).

--- New_task_7.py
+++ New_task_7.py
@@ -2,10 +2,11 @@
 X = [False, True]
 Y = [False, True]
 Z = [False, True]
+print('X\t\tY\t\tZ\t\tResult')
 for i in X:
     for j in Y:
         for k in Z:
             if (not(X or Y or Z)) == (not X and not Y and not Z):
-                print(True)
+                print(f'{i}\t{j}\t{k}\tTrue')
             else:
-                print(False)
+                print(f'{i}\t{j}\t{k}\tFalse')
