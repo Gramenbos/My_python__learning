838691e743718ceb9bea4a8008b20fcba57ac95a
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-03-09 17:44:51 +0300

    New task 13 done (The sum of the numbers - recursion).

---
+++ New_task_13.py
@@ -0,0 +1,12 @@
+# Сложите цифры целого числа.
+# Решил использовать рекурсию!!!
+def sum_numbers(number):
+    if number < 10:
+        return number
+    else:
+        return number % 10 + sum_numbers(number // 10)
+
+
+num = int(input('Enter any number: '))
+result = sum_numbers(num)
+print(f'The sum of the numbers is {result}.')
