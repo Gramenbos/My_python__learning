5cb96cf5d2f588e303a0719b812574fcea2a95d4
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-03-10 17:00:04 +0300

    Lesson 1 (Грокаем алгоритмы)

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,5 +1,8 @@
 # Задания из книги "Грокаем алгоритмы"
 # Сортировка
+from collections import deque
+
+
 def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
@@ -51,3 +54,33 @@ def check_voter(name):
 check_voter('Tom')
 check_voter('Mike')
 check_voter('Mike')
+
+# Задача про продавца манго
+graph = {"you": ["alice", "bob", "claire"],
+         "bob": ["anuj", "peggy"],
+         "alice": ["peggy"],
+         "claire": ["thom", "jonny"],
+         "anuj": [], "peggy": [], "thom": [], "jonny": []}
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + ' is a mango seller')
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+search('you')
